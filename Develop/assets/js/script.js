b3383cfbac451be54b20143c1c601dd2c933fe66
Alexis Langille <langille.alexis@gmail.com>
2024-06-20 21:28:42 -0400

    Render Task List on Page Load
    This creates the structure for a new task with all the attributes, and
    adds it to the taskList to eventually store in localstorage.

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -14,7 +14,20 @@ function createTaskCard(task) {
 
 // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {
+    const title = $('#taskTitle').val();
+    const dueDate = $('#taskDueDate').val();
+    const description = $('#taskDescription').val();
 
+    const newTask = {
+        id: generateTaskId(),
+        title,
+        dueDate,
+        description,
+        status: 'todo'
+    };
+
+    console.log("Adding new task:", newTask);
+    taskList.push(newTask);
 }
 
 // Todo: create a function to handle adding a new task
@@ -34,5 +47,6 @@ function handleDrop(event, ui) {
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
-
+    console.log("Document ready, initializing..."); 
+    renderTaskList();
 });
